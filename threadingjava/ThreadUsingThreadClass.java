ee3a8bd9ac738966e70bfa490e1acbd3efb22aff
Tazerface <231075017+tazerface007@users.noreply.github.com>
2026-04-30 18:11:04 +0000

    Add example of Threading using Thread class

--- threadingjava/ThreadUsingThreadClass.java
+++ threadingjava/ThreadUsingThreadClass.java
@@ -2,29 +2,38 @@ import java.lang.Thread;
 
 public class ThreadUsingThreadClass extends Thread {
 
-	private String name;
+private String name;
+private int seconds;
 
-	public ThreadUsingThreadClass(String name) {
-		this.name = name;
+public ThreadUsingThreadClass(String name, int seconds) {
+	this.name = name;
+	this.seconds = seconds;
+}
+
+@Override
+public void run() {
+	try{
+	Thread.sleep(seconds*1000);
+	System.out.println("Thread " + name + " running.");
 	}
-	
-	@Override
-	public void run() {
-		System.out.println("Thread " + name + " running.");
-
+	catch(InterruptedException e) {
+		e.printStackTrace();
 	}
 
-	public static void main(String[] args) {
-		ThreadUsingThreadClass t1 = new ThreadUsingThreadClass("Thread 1");
-		ThreadUsingThreadClass t2 = new ThreadUsingThreadClass("Thread 2");
-		ThreadUsingThreadClass t3 = new ThreadUsingThreadClass("Thread 3");
-		ThreadUsingThreadClass t4 = new ThreadUsingThreadClass("Thread 4");
-		t1.start();
-		t2.start();
-		t3.start();
-		t4.start();
+
+}
+
+public static void main(String[] args) {
+	ThreadUsingThreadClass t1 = new ThreadUsingThreadClass("Thread 1", 2);
+	ThreadUsingThreadClass t2 = new ThreadUsingThreadClass("Thread 2", 4);
+	ThreadUsingThreadClass t3 = new ThreadUsingThreadClass("Thread 3", 1);
+	ThreadUsingThreadClass t4 = new ThreadUsingThreadClass("Thread 4", 6);
+	t1.start();
+	t2.start();
+	t3.start();
+	t4.start();
 
 
-	}
+}
 
 }
